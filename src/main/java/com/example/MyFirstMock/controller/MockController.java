461d315149d9f8c51c4e1f53d7b614a79cafb5ab
vdp <djsegx@gmail.com>
2024-03-07 15:33:28 +0600

    file interaction has been added

--- src/main/java/com/example/MyFirstMock/controller/MockController.java
+++ src/main/java/com/example/MyFirstMock/controller/MockController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.sql.SQLException;
 import java.util.Map;
 
 @RestController
@@ -13,19 +15,17 @@ public class MockController {
     UserService userService = new UserService();
 
     @GetMapping("/user")
-    ResponseEntity<?> getUser(@RequestParam(required = true) String login) {
+    ResponseEntity<?> getUser(@RequestParam(required = true) String login) throws SQLException {
         return ResponseEntity.ok(userService.getUserByLogin(login).toString());
+    }
+
+    @GetMapping("/file")
+    ResponseEntity<?> getUserFromFile() {
+        return ResponseEntity.ok(userService.getUserFromFile());
     }
 
     @PostMapping(value = "/user", consumes = {"application/json"})
     ResponseEntity<?> postUser(@RequestBody Map<String, String> user) {
-//        try {
-//            if ((user.size() > 3) || (!user.containsKey("login") || (!user.containsKey("password")) || (!user.containsKey("email")) || (user.containsValue(""))))
-//                throw new Exception("Bad JSON");
-//            else return ResponseEntity.ok(userService.setUser(new User(user.get("login"), user.get("password"), user.get("email"))));
-//        } catch (Exception e) {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-//        }
         if ((user.size() > 3) || (!user.containsKey("login") || (!user.containsKey("password")) || (!user.containsKey("email")) || (user.containsValue(""))))
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad JSON");
         else return ResponseEntity.ok(userService.setUser(new User(user.get("login"), user.get("password"), user.get("email"))));
